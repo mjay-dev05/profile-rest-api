78fd9d9ecff5d8f4ef6b00dc40e96cd0cf1671cf
Mrityunjay Pandey <mrityunjaypandey05@gmail.com>
2025-09-30 21:11:04 +0530

    Added django cmds

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import response
 
-# Create your views here.
+class HelloApiView(APIView):
+    
